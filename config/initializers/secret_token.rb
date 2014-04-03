db2e1708912878ab8ea9aa82266f559d51ef92e5
Takashi Kiyose <takashi.kiyose@resupress.com>
2014-04-03 19:52:11 +0900

    initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+KiyosePractice::Application.config.secret_key_base = '430338bae208fda49afd52e4b51100cb2566eaa4b8bdf5ca62b9441f9984c219084ddcaafc9c852b6eb9fc1ec65491431e79e964865a2796dbe65ab17757a9d7'
